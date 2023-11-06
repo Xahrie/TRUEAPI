d0fff6f614688a30dcaf0a7cfdce29690c97a3f7
Xahrie <martin.zeyner@web.de>
2023-11-06 11:45:22 +0100

    3.3.43:
    Bugfixing

--- src/main/java/de/xahrie/trues/api/util/io/cfg/JSON.java
+++ src/main/java/de/xahrie/trues/api/util/io/cfg/JSON.java
@@ -2,20 +2,28 @@ package de.xahrie.trues.api.util.io.cfg;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.HashMap;
 
 import lombok.NonNull;
 import org.json.JSONObject;
 
 public final class JSON extends JSONObject {
+  private static FileSystem currentFileSystem;
+
   public static JSON read(@NonNull String fileName) {
     try {
       final String content = Files.readString(getPath(fileName));
+      closeResource();
       return new JSON(content);
     } catch (IOException | URISyntaxException e) {
       throw new RuntimeException(e);
@@ -33,21 +41,22 @@ public final class JSON extends JSONObject {
   private static void doWrite(String fileName, String content, StandardOpenOption mode) {
     try {
       Files.writeString(getPath(fileName), content, mode, StandardOpenOption.CREATE);
+      closeResource();
     } catch (IOException | URISyntaxException e) {
       throw new RuntimeException(e);
     }
   }
 
   @NonNull
-  static Path getPath(@NonNull String fileName) throws URISyntaxException, FileNotFoundException {
+  static Path getPath(String fileName) throws URISyntaxException, FileNotFoundException {
     if (Files.exists(Paths.get("./resources/"))) {
       return Paths.get("./resources/" + fileName);
     } else {
-      URL resource = JSON.class.getResource("/" + fileName);
+      URL resource = JSON.class.getResource("/" + fileName); // for jars
       if (resource == null) {
         resource = JSON.class.getResource(fileName);
         if (resource == null) {
-          resource = JSON.class.getClassLoader().getResource("/" + fileName);
+          resource = JSON.class.getClassLoader().getResource("/" + fileName); // for jars
           if (resource == null) {
             resource = JSON.class.getClassLoader().getResource(fileName);
             if (resource == null) {
@@ -56,7 +65,26 @@ public final class JSON extends JSONObject {
           }
         }
       }
-      return Path.of(resource.toURI());
+
+      try {
+        return Path.of(resource.toURI());
+      } catch (FileSystemNotFoundException fileSystemException) { // for jars
+        final String[] array = resource.toURI().toString().split("!");
+        try {
+          currentFileSystem = FileSystems.newFileSystem(URI.create(array[0]), new HashMap<>());
+          return currentFileSystem.getPath(array[1]);
+        } catch (IOException exception) {
+          throw new FileNotFoundException("File " + fileName + " konnte nicht gefunden werden.");
+
+        }
+      }
+    }
+  }
+
+  private static void closeResource() throws IOException {
+    if (currentFileSystem != null) {
+      currentFileSystem.close();
+      currentFileSystem = null;
     }
   }
 
